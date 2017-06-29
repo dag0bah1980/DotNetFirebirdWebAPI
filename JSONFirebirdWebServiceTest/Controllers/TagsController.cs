fe4e75682261059e97963f22989b92adde22e673
dag0bah1980 <clorico@gmail.com>
2017-06-29 00:26:34 -0400

    Added a new route to check if a code already exists
    Wanted to use this route to precheck before submit but decided that's a
    nice to have for now. Left the route in case I need it later.

--- JSONFirebirdWebServiceTest/Controllers/TagsController.cs
+++ JSONFirebirdWebServiceTest/Controllers/TagsController.cs
@@ -174,6 +174,87 @@ namespace JSONFirebirdWebServiceTest.Controllers
             }
         }
 
+        [Route("Tags/CodeMatch/{requestedcode}")]
+        [HttpGet]
+        // GET: api/Users/5
+        public IHttpActionResult Get(string requestedcode)
+        {
+            DBConnection fbconndetails = new DBConnection();
+
+            Connection selectconnection = new Connection(fbconndetails.DBHost, string.Concat(fbconndetails.DBPath, fbconndetails.DBFile), Convert.ToInt32(fbconndetails.DBPort), fbconndetails.DBUser, fbconndetails.DBPassword, fbconndetails.DBConnectionLifeTime, fbconndetails.DBPooling, fbconndetails.DBMinPoolSize, fbconndetails.DBMaxPoolSize);
+            DataTable result = new DataTable();
+
+            JSONObject jbuilder = new JSONObject();
+            JSONObject.MetaDetails mbuilder = new JSONObject.MetaDetails();
+
+            string status = "";
+            string code = "";
+            string message = "";
+
+            string sqlcmd = "select * from codecheck_tag(@requestedcode)";
+
+            using (selectconnection.fbconnect)
+            {
+
+                try
+                {
+                    selectconnection.fbconnect.Open();
+                    FbTransaction fbtrans = selectconnection.fbconnect.BeginTransaction();
+                    FbParameter codeParam = new FbParameter("@requestedcode", FbDbType.VarChar);
+                    codeParam.Value = requestedcode;
+
+                    FbCommand fbcmd = new FbCommand(sqlcmd, selectconnection.fbconnect, fbtrans);
+                    fbcmd.Parameters.Add(codeParam);
+
+
+                    using (FbDataReader fbsqlreader = fbcmd.ExecuteReader())
+                    {
+                        try
+                        {
+                            result.Load(fbsqlreader);
+                            status = "Success";
+                            code = "200";
+                            message = "All Good!";
+                        }
+                        catch (Exception e)
+                        {
+                            ExceptionsLogByFile logger = new ExceptionsLogByFile();
+                            logger.LogException(e);
+                            status = "Fail";
+                            code = "500";
+                            message = "Internal Server Error";
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    ExceptionsLogByFile logger = new ExceptionsLogByFile();
+                    logger.LogException(ex);
+                    status = "Fail";
+                    code = "503";
+                    message = "Internal Server Error - Cannot access database";
+                }
+                finally
+                {
+
+                }
+                selectconnection.fbconnect.Close();
+
+
+                mbuilder.status = status;
+                mbuilder.code = code;
+                mbuilder.message = message;
+
+                jbuilder.Meta = mbuilder;
+
+                jbuilder.Data = result;
+
+                return Json(jbuilder);
+
+            }
+        }
+
         [Route("Tags")]
         [HttpPost]
         // POST: api/Tags
@@ -274,9 +355,12 @@ namespace JSONFirebirdWebServiceTest.Controllers
             string code = "";
             string message = "";
 
+            string updatedTagBodyTagCode = updatedTag.TAG;
+
             string sqlcmd = "update tag set " +
-                "MODIFIED=@MODIFIED " +
-                "WHERE ID = @ID";
+                "WHERE " + 
+                "ID = @ID and" +
+                "TAG = @TAG";
 
 
             using (selectconnection.fbconnect)
@@ -287,18 +371,14 @@ namespace JSONFirebirdWebServiceTest.Controllers
                     selectconnection.fbconnect.Open();
                     FbTransaction fbtrans = selectconnection.fbconnect.BeginTransaction();
                     FbParameter idParam = new FbParameter("@ID", FbDbType.BigInt);
-                    FbParameter modifiedParam = new FbParameter("@MODIFIED", FbDbType.TimeStamp);
-
+                    FbParameter tagParam = new FbParameter("@TAG", FbDbType.VarChar);
 
                     idParam.Value = updatedId;
-                    modifiedParam.Value = DateTime.Now.ToString();
-
+                    tagParam.Value = updatedTagBodyTagCode;
 
                     FbCommand fbcmd = new FbCommand(sqlcmd, selectconnection.fbconnect, fbtrans);
                     fbcmd.Parameters.Add(idParam);
-                    fbcmd.Parameters.Add(modifiedParam);
-
-
+                    fbcmd.Parameters.Add(tagParam);
 
                     fbcmd.ExecuteNonQuery();
                     fbtrans.Commit();
