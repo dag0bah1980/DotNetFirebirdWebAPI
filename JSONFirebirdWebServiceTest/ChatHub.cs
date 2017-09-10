baa37dbb0ad1ad574e77e62d002ca405fe94df2b
dag0bah1980 <clorico@gmail.com>
2017-09-10 06:54:13 -0400

    Got a sample of SignalR working on the WebAPI
    Prototyped a local chat function in the WebAPI. Will now try to get it
    to work in the Angular App.

--- JSONFirebirdWebServiceTest/ChatHub.cs
+++ JSONFirebirdWebServiceTest/ChatHub.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
-
-namespace JSONFirebirdWebServiceTest.SignalR
+namespace JSONFirebirdWebServiceTest
 {
-    public class ChatHub: Hub
+    public class ChatHub : Hub
     {
         public void Send(string name, string message)
         {
